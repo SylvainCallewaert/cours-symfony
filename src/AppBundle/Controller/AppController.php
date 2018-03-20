691424d7b5d9673cb9e601c24c81f402dc789061
formation <formateur@mail.com>
2018-03-20 13:40:03 +0100

    utilisation entité

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Article;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,29 @@ class AppController extends Controller
      */
     public function accueilAction(Request $request)
     {
-        return $this->render('app/accueil.html.twig');
+        // instanciation manuelle des articles
+        $article = new Article();
+        $article->setTitle("Titre dynamique article");
+        $article->setDescription("<p style='color:red;'>Mon paragraphe article en rouge</p>");
+        $article->setAuteur("Fabian");
+        $now = new \DateTime();
+        $article->setCreatedAt($now);
+
+        $article2 = new Article();
+        $article2->setTitle("Titre article 2");
+        $article2->setDescription("Paragraphe 2");
+        $article2->setAuteur("Fab");
+        $now = new \DateTime();
+        $article2->setCreatedAt($now);
+
+        // on met les deux objets articles dans un tableau "articles"
+        $articles[] = $article2;
+        $articles[] = $article;
+
+        // on passe le tableau à la vue
+        return $this->render('app/accueil.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 
     /**
---
+++ src/AppBundle/Entity/Article.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Article
+ */
+class Article
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $auteur;
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdAt.
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Article
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt.
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set title.
+     *
+     * @param string $title
+     *
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description.
+     *
+     * @param string $description
+     *
+     * @return Article
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set auteur.
+     *
+     * @param string $auteur
+     *
+     * @return Article
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * Get auteur.
+     *
+     * @return string
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+}
