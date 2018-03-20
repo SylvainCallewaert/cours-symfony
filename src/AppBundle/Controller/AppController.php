8d2857a6eb935b3d0fd2d16dd8cbd8e06e6eac47
formation <formateur@mail.com>
2018-03-20 16:44:25 +0100

    CRUD article avec doctrine

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\User\User;
 
 class AppController extends Controller
 {
@@ -96,12 +97,96 @@ class AppController extends Controller
         return $this->render('app/article1.html.twig', ['article' => $article]);
     }
 
+
+    /**
+     * @Route("/article/ajout", name="article_ajout")
+     */
+    public function insertArticleAction()
+    {
+        // création instance d'un article manuellement
+        $article = new Article();
+        $article->setCreatedAt(new \DateTime());
+        $article->setTitle("Titre article inséré via doctrine");
+        $article->setDescription("J'ai été enregistré grâce à l'entity manager de doctrine");
+        $article->setAuteur('fab');
+
+        // enregistrement en base grâce à doctrine
+        $em = $this->getDoctrine()->getManager();
+        // persist pour dire à doctrine d'enregistrer l'objet lors du flush
+        $em->persist($article);
+
+        // flush : execute les requetes pour insérer tous les objets
+        // sur lesquels on a préalablement passé à la méthode persist de $em
+        $em->flush();
+
+        // afficher à l'écran l'ID de l'article qui vient d'être créé
+        // cet ID est immédiatement disponible après l'appel à $em->flush()
+        // grâce au getter getId() de l'objet persisté
+        return new Response($article->getId());
+    }
+
+    /**
+     * @Route("/article/update/{id}", name="article_update")
+     */
+    public function updateArticleAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $article = $em->getRepository('AppBundle:Article')->find($id);
+
+        if (!$article instanceof Article) {
+            throw new NotFoundHttpException();
+        }
+
+        $article->setTitle('Titre article 1 Modifié');
+
+        // persist pour dire à doctrine de modifier l'objet lors du flush
+        // persist est utilisé pour les insert et les update
+        // la différence se fait grâce à l'ID de l'objet
+        // qui est soit null (insert), soit setté (update)
+        $em->persist($article);
+
+        // flush : execute les requetes pour modifier tous les objets
+        // sur lesquels on a préalablement passé à la méthode persist de $em
+        $em->flush();
+
+        // afficher à l'écran l'ID de l'article qui vient d'être créé
+        // cet ID est immédiatement disponible après l'appel à $em->flush()
+        // grâce au getter getId() de l'objet persisté
+        return new Response($article->getId());
+    }
+
+    /**
+     * @Route("/article/delete/{id}", name="article_delete")
+     */
+    public function deleteArticleAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository('AppBundle:Article')->find($id);
+
+        if (!$article instanceof Article) {
+            return new Response("L'article n'existe pas.");
+        }
+
+        // remove pour dire à doctrine de supprimer l'objet en bdd
+        // lors du flush
+        $em->remove($article);
+
+        // flush : execute les requetes pour supprimer tous les objets
+        // sur lesquels on a préalablement passé à la méthode remove de $em
+        $em->flush();
+
+        // après cette ligne, $article->getId() renvoie NULL
+
+        return new Response("L'article ".$id." a bien été supprimé");
+    }
+
     /**
      * @Route("/article/{id}", name="article_affichage")
      */
     public function articleAction(Request $request, $id)
     {
-        // récupération de l'article en BDDD qui a l'ID 1
+        // récupération de l'article en BDD qui a l'ID 1
 
         $em = $this->getDoctrine()->getManager();
         $article = $em->getRepository('AppBundle:Article')->find($id);
@@ -112,4 +197,11 @@ class AppController extends Controller
 
         return $this->render('app/article.html.twig', ['article' => $article]);
     }
+
+    /**
+     *
+     * Dans le template jeu.html.twig, créer un lien de suppression pour chaque article
+     *
+     */
+
 }
