4cd17253328fdae3b303e0a240e8ac71987a3912
formation <formateur@mail.com>
2018-03-20 10:23:28 +0100

    exo1 symfony

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -8,5 +8,27 @@ use Symfony\Component\HttpFoundation\Request;
 
 class AppController extends Controller
 {
+    /**
+     * @Route("/accueil", name="nom_page_accueil")
+     */
+    public function accueilAction(Request $request)
+    {
+        return $this->render('app/accueil.html.twig');
+    }
 
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        return $this->render('app/login.html.twig');
+    }
+
+    /**
+     * @Route("/jeu", name="jeu")
+     */
+    public function jeuAction(Request $request)
+    {
+        return $this->render('app/jeu.html.twig');
+    }
 }
